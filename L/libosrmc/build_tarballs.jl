b7f0ff9ddcc343e0cd57b234779add2e039dab05
Jonas Klasen <jrklasen@pm.me>
2025-11-23 13:19:35 +0100

    latest updates to libosrmc

--- L/libosrmc/build_tarballs.jl
+++ L/libosrmc/build_tarballs.jl
@@ -4,31 +4,50 @@ name = "libosrmc"
 version = v"6.0.0"
 
 sources = [
+    DirectorySource("/home/jrklasen/dev/moviro/libosrmc"),
     ArchiveSource(
-        "https://github.com/jrklasen/libosrmc/archive/refs/heads/osrm-6.tar.gz",
-        "4f60dc352fae87a5d83e9c3db7cfb2f4d7fb26d2e56bbb0af2629a4caf2edc7d",
+        "https://github.com/roblabla/MacOSX-SDKs/releases/download/13.3/MacOSX13.3.sdk.tar.xz",
+        "e5d0f958a079106234b3a840f93653308a76d3dcea02d3aa8f2841f8df33050c",
     ),
 ]
 
 script = raw"""
-cd ${WORKSPACE}/srcdir/libosrmc-osrm-6/libosrmc
+cd ${WORKSPACE}/srcdir
+[[ -d "libosrmc/libosrmc" ]] && cd libosrmc/libosrmc || cd libosrmc
 
 export PKG_CONFIG_PATH="${prefix}/lib/pkgconfig:${prefix}/share/pkgconfig:${PKG_CONFIG_PATH}"
 export CPPFLAGS="-I${prefix}/include ${CPPFLAGS}"
-export LDFLAGS="-L${prefix}/lib ${LDFLAGS}"
+if [[ "${target}" == *-mingw* ]]; then
+    export CPPFLAGS="-I${prefix}/include/osrm ${CPPFLAGS}"
+fi
+export LDFLAGS="-L${prefix}/lib -Wl,-rpath,${prefix}/lib ${LDFLAGS}"
 
-make -j${nproc} \
-    PREFIX=${prefix} \
-    VERSION_MAJOR=6 \
-    VERSION_MINOR=0
+if [[ -n "${LD_LIBRARY_PATH}" ]]; then
+    export LD_LIBRARY_PATH=$(echo "${LD_LIBRARY_PATH}" | tr ':' '\n' | grep -v "destdir" | tr '\n' ':' | sed 's/:$//; s/^://')
+fi
 
-make install \
-    PREFIX=${prefix} \
-    VERSION_MAJOR=6 \
-    VERSION_MINOR=0
+if [[ "${target}" == *-apple-* ]]; then
+    if [[ "${target}" == x86_64-apple-darwin* ]]; then
+        export MACOSX_DEPLOYMENT_TARGET=11.0
+        export EXTRA_CXXFLAGS="-mmacosx-version-min=11.0"
+        SDK_TAR=$(find ${WORKSPACE}/srcdir -name "MacOSX*.sdk.tar.xz" | head -1)
+        [[ -f "${SDK_TAR}" ]] && tar --extract --file="${SDK_TAR}" --directory=/opt/${target}/${target}/sys-root/. --strip-components=1 MacOSX13.3.sdk/System MacOSX13.3.sdk/usr 2>/dev/null || true
+    else
+        export MACOSX_DEPLOYMENT_TARGET=11.0
+        export EXTRA_CXXFLAGS="-mmacosx-version-min=11.0"
+    fi
+else
+    export EXTRA_CXXFLAGS=""
+fi
+
+make clean
+make -j${nproc} PREFIX=${prefix} VERSION_MAJOR=6 VERSION_MINOR=0 EXTRA_CXXFLAGS="${EXTRA_CXXFLAGS}"
+make install PREFIX=${prefix} VERSION_MAJOR=6 VERSION_MINOR=0
 """
 
-platforms = [Platform("x86_64", "linux"; cxxstring_abi = "cxx11")]
+platforms = supported_platforms()
+platforms = filter(p -> (Sys.isapple(p) || Sys.islinux(p)) && !(Sys.isapple(p) && arch(p) == "x86_64"), platforms)
+platforms = expand_cxxstring_abis(platforms)
 
 products = [
     LibraryProduct("libosrmc", :libosrmc; dont_dlopen = true),
@@ -36,8 +55,12 @@ products = [
 ]
 
 dependencies = [
-    Dependency(PackageSpec(name = "OSRM_jll", url = "https://github.com/jrklasen/OSRM_jll.jl")),
     Dependency("CompilerSupportLibraries_jll"),
+    Dependency(PackageSpec(name="OSRM_jll", url="https://github.com/jrklasen/OSRM_jll.jl", rev="main")),
+    Dependency("boost_jll"),
+    Dependency("Expat_jll"; compat="2.6.5"),
+    Dependency("Zlib_jll"),
+    Dependency("Bzip2_jll"),
 ]
 
 build_tarballs(
@@ -49,5 +72,6 @@ build_tarballs(
     platforms,
     products,
     dependencies;
+    julia_compat="1.10",
     preferred_gcc_version = v"13",
 )
